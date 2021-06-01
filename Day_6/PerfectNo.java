c047d27a45d00a212ca98ee57b40df7d939a2727
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-06-01 16:07:20 +0530

    Updated Day6

--- Day_6/PerfectNo.java
+++ Day_6/PerfectNo.java
@@ -5,17 +5,17 @@ public class PerfectNo {
         long sum = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number : ");
-        int no = sc.nextLong();
+        int no = sc.nextInt();
         int i = 1;
-        while (i <= n / 2) {
-            if (n % i == 0) {
+        while (i <= no / 2) {
+            if (no % i == 0) {
                 sum += i;
             }
             i++;
         }
-        if (sum == n) {
-            System.out.println(n + " is a perfect number");
+        if (sum == no) {
+            System.out.println(no + " is a perfect number");
         } else
-            System.out.println(n + " is not a  perfect number");
+            System.out.println(no + " is not a  perfect number");
     }
 }
